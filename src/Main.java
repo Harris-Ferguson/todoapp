e79c4eb2f20efb4690486d4a67cc825a4eb9e6db
Harris Ferguson <haf094@usask.ca>
2023-03-15 20:06:08 -0600

    added list and add functions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+    static private ArrayList<String> todos = new ArrayList<String>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the Todo App!");
+        for(;;){
+            System.out.println("Select an operation");
+            System.out.print("1. Add A TODO");
+            System.out.print("2. List all TODOS");
+            // TODO ADD THE REST LOL
+            Scanner scanner = new Scanner(System.in);
+            int selection = Integer.parseInt(scanner.nextLine());
+            if(selection == 1){
+                System.out.println("Input a new todo");
+                String newTodo = scanner.nextLine();
+                Main.todos.add(newTodo);
+            }
+            if (selection == 2) {
+                System.out.println(todos);
+            }
+        }
     }
 }
