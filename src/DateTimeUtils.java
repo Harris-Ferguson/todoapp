67b1f1db1c764e7427be9a25cc0edb6793635197
Harris Ferguson (haf094) <haf094@mail.usask.ca>
2023-03-22 18:03:42 -0600

    added datetime utility function and extracted parsing logic into it

---
+++ src/DateTimeUtils.java
@@ -0,0 +1,48 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTimeUtils {
+
+    private static String[] allowed =
+            {
+                    "yyyy.MM.dd G 'at' HH:mm:ss z",
+                    "EEE, MMM d, ''yy",
+                    "h:mm a",
+                    "hh 'o''clock' a, zzzz",
+                    "K:mm a, z",
+                    "yyyyy.MMMMM.dd GGG hh:mm aaa",
+                    "EEE, d MMM yyyy HH:mm:ss Z",
+                    "yyMMddHHmmssZ",
+                    "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
+                    "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
+                    "YYYY-'W'ww-u",
+                    "EEE, dd MMM yyyy HH:mm:ss z",
+                    "EEE, dd MMM yyyy HH:mm zzzz",
+                    "yyyy-MM-dd'T'HH:mm:ssZ",
+                    "yyyy-MM-dd'T'HH:mm:ss.SSSzzzz",
+                    "yyyy-MM-dd'T'HH:mm:sszzzz",
+                    "yyyy-MM-dd'T'HH:mm:ss z",
+                    "yyyy-MM-dd'T'HH:mm:ssz",
+                    "yyyy-MM-dd'T'HH:mm:ss",
+                    "yyyy-MM-dd'T'HHmmss.SSSz",
+                    "yyyy-MM-dd",
+                    "yyyyMMdd",
+                    "dd/MM/yy",
+                    "dd/MM/yyyy"
+            };
+
+    public static Date smartParseDate(String dateString)
+    {
+        Date date = new Date();
+        for (String format : allowed) {
+            SimpleDateFormat simpleFormat = new SimpleDateFormat(format);
+            try {
+                date = simpleFormat.parse(dateString);
+            } catch (ParseException e) {
+
+            }
+        }
+        return date;
+    }
+}
--- src/TodoApp.java
+++ src/TodoApp.java
@@ -26,49 +26,13 @@ public class TodoApp {
                 System.out.println("Input a new todo description");
                 String newTodoDescription = scanner.nextLine();
                 System.out.println("Due date? Leave blank for none");
-                // delete this
-                Date outputDate = null;
-                String[] allowed =
-                        {
-                                "yyyy.MM.dd G 'at' HH:mm:ss z",
-                                "EEE, MMM d, ''yy",
-                                "h:mm a",
-                                "hh 'o''clock' a, zzzz",
-                                "K:mm a, z",
-                                "yyyyy.MMMMM.dd GGG hh:mm aaa",
-                                "EEE, d MMM yyyy HH:mm:ss Z",
-                                "yyMMddHHmmssZ",
-                                "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
-                                "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
-                                "YYYY-'W'ww-u",
-                                "EEE, dd MMM yyyy HH:mm:ss z",
-                                "EEE, dd MMM yyyy HH:mm zzzz",
-                                "yyyy-MM-dd'T'HH:mm:ssZ",
-                                "yyyy-MM-dd'T'HH:mm:ss.SSSzzzz",
-                                "yyyy-MM-dd'T'HH:mm:sszzzz",
-                                "yyyy-MM-dd'T'HH:mm:ss z",
-                                "yyyy-MM-dd'T'HH:mm:ssz",
-                                "yyyy-MM-dd'T'HH:mm:ss",
-                                "yyyy-MM-dd'T'HHmmss.SSSz",
-                                "yyyy-MM-dd",
-                                "yyyyMMdd",
-                                "dd/MM/yy",
-                                "dd/MM/yyyy"
-                        };
-                String input = scanner.nextLine();
-                Date date = new Date();
-                for (String format : allowed) {
-                    SimpleDateFormat simpleFormat = new SimpleDateFormat(format);
-                    try {
-                        date = simpleFormat.parse(input);
-                    } catch (ParseException e) {
 
-                    }
-                }
+                String input = scanner.nextLine();
+                Date dueDate = DateTimeUtils.smartParseDate(input);
                 Todo newTodo = new Todo(
                         newTodoDescription,
                         (new Date()).toString(),
-                        date.toString(),
+                        dueDate.toString(),
                         false
                 );
                 todos.add(newTodo);
